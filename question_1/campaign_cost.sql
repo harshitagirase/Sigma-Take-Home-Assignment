a7843869f6d1c7d61d02d240b3b617bc0a573e89
Harshita Girase <harshitagirase@Harshitas-MacBook-Pro.local>
2023-03-18 12:44:36 -0700

    Rename fields

--- question_1/campaign_cost.sql
+++ question_1/campaign_cost.sql
@@ -10,7 +10,7 @@ source as (
       name as campaign_name,
       type as campaign_type,
       actual_cost,
-      number_of_responses,
+      number_of_responses as responded,
       start_date,
       end_date
     from applications.salesforce.campaign
@@ -20,13 +20,13 @@ final as (
     select
       campaign_name,
       campaign_type,
-      number_of_responses,
+      responded,
       actual_cost,
-      actual_cost / number_of_responses as campaign_cost
+      actual_cost / number_of_responses as avg_cost_per_attendee
     from source
     where start_date >= '2022-01-01' -- we only want to compare all campaigns that ran since 2022
 )
   
 select * from final
-order by campaign_cost desc
+order by avg_cost_per_attendee desc
 limit 10
