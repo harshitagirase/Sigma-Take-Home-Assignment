f993ffe436b9c459adff17d4ce877ed84523b136
Harshita Girase <harshitagirase97@gmail.com>
2023-03-19 22:25:00 -0700

    Add comments

--- question_3/closed_won_opportunities.sql
+++ question_3/closed_won_opportunities.sql
@@ -9,12 +9,13 @@ final as (
         name,
         type,
         amount as sum_amount,
+        -- this will help identify the la
         row_number() over (
             partition by name
             order by close_date desc)
         as row_num
     from applications.salesforce.opportunity
-    where stage_name = 'Closed Won'
+    where stage_name = 'Closed Won' -- we only care about opportunities that are in the 'Closed Won' stage
 
 )
 
@@ -23,4 +24,8 @@ select
     type,
     sum_amount
 from final
+/* in the final CTE, the latest record for an opportunity (determined by latest `close_date`) will be asigned a value of 1 
+in the `row_num` column. Since we want to get the latest opportunity, we filter our result to only include record with row_num = 1
+*/
 where row_num = 1
+order by 1, 2
